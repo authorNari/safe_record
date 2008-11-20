6ab0fe52d6809e753accf1381da488c91e8878d8
Narihiro Nakamura <authorNari@gmail.com>
2008-11-20 15:22:50 +0900

    updated postgre_adaptor adhock correct.

--- lib/adhock/rails_2.rb
+++ lib/adhock/rails_2.rb
@@ -4,12 +4,12 @@ module ActiveRecord
   module ConnectionAdapters
     class PostgreSQLAdapter
       def client_min_messages_with_quote=(level)
-        client_min_messages_without_quote = h(level).untaint
+        execute("SET client_min_messages TO #{quote(level)}")
       end
       alias_method_chain(:client_min_messages=, :quote)
 
       def last_insert_id_with_quote(table, sequence_name)
-        last_insert_id_without_quote(table, h(sequence_name).untaint)
+        Integer(select_value("SELECT currval(#{quote(sequence_name)})"))
       end
       alias_method_chain(:last_insert_id, :quote)
     end
